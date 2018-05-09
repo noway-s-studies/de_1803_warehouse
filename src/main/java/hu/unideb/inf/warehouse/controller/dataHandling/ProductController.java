31ce85a86c553f0bb6cf729abc1795249238302b
noway0032 <noway0032@gmail.com>
2018-05-09 21:01:26 +0200

    ProductTableOk

--- src/main/java/hu/unideb/inf/warehouse/controller/dataHandling/ProductController.java
+++ src/main/java/hu/unideb/inf/warehouse/controller/dataHandling/ProductController.java
@@ -1,7 +1,148 @@
 package hu.unideb.inf.warehouse.controller.dataHandling;
 
+import hu.unideb.inf.warehouse.model.ProductModel;
+import hu.unideb.inf.warehouse.pojo.Product;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.input.MouseEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class ProductController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ProductController implements Initializable {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+    private ProductModel pm;
+    private ObservableList<Product> data = FXCollections.observableArrayList();
+    private Product selectedProduct = null;
 
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        pm = new ProductModel();
+        table.getColumns().removeAll();
+        updateTableData();
+    }
+
+    @FXML
+    TableView table;
+    @FXML
+    TextField inputLabel;
+    @FXML
+    TextField inputUnitLabel;
+    @FXML
+    Button cleanProductTextFieldButton;
+    @FXML
+    Button addProductButton;
+    @FXML
+    Button delProductButton;
+    @FXML
+    Button modProductButton;
+    @FXML
+    private TableColumn<Product, String> labelColumn = null;
+    @FXML
+    private TableColumn<Product, String> unitLabelColumn = null;
+
+    @FXML
+    public void actionCleanProductTextField(MouseEvent event){
+        inputLabel.clear();
+        inputUnitLabel.clear();
+        cleanProductTextFieldButton.setVisible(true);
+        addProductButton.setVisible(true);
+        delProductButton.setVisible(false);
+        modProductButton.setVisible(false);
+        selectedProduct = null;
+    }
+
+    @FXML
+    public void actionDelProductContact(MouseEvent event){
+        if (selectedProduct != null){
+            pm.removeProduct(selectedProduct);
+            actionCleanProductTextField(event);
+            updateTableData();
+        }
+    }
+
+    @FXML
+    public void actionModProductContact(MouseEvent event){
+        if (selectedProduct != null){
+            selectedProduct.setLabel(inputLabel.getText());
+            selectedProduct.setUnitLabel(inputUnitLabel.getText());
+
+            pm.modProduct(selectedProduct);
+            actionCleanProductTextField(event);
+            updateTableData();
+        }
+    }
+
+    @FXML
+    public void actionAddProductContact(MouseEvent event){
+        if (inputLabel != null && inputUnitLabel != null){
+            Product newPureyor = new Product(
+                    inputLabel.getText(),
+                    inputUnitLabel.getText()
+            );
+            data.add(newPureyor);
+            pm.addProduct(newPureyor);
+            inputLabel.clear();
+            inputUnitLabel.clear();
+            log.info("Új beszerzőt betőltve");
+        }
+    }
+
+    private void numberMaxMinTextFieldListener(TextField tf, int min, int max) {
+        tf.setTextFormatter(new TextFormatter<Integer>(change -> {
+            if (change.isDeleted()) {
+                return change;
+            }
+            String txt = change.getControlNewText();
+            if (txt.matches("0\\d+")) {
+                return null;
+            }
+            try {
+                int n = Integer.parseInt(txt);
+                return min <= n && n <= max ? change : null;
+            } catch (NumberFormatException e) {
+                return null;
+            }
+        }));
+    }
+
+    private void updateTableData() {
+        table.getItems().clear();
+        table.getColumns().clear();
+
+        labelColumn = new TableColumn("Megnevezés");
+        labelColumn.setMinWidth(200);
+        unitLabelColumn = new TableColumn("Mértékegység");
+        unitLabelColumn.setMinWidth(100);
+
+        labelColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        labelColumn.setCellValueFactory(new PropertyValueFactory<Product, String>("label"));
+        unitLabelColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        unitLabelColumn.setCellValueFactory(new PropertyValueFactory<Product, String>("unitLabel"));
+
+        table.getColumns().addAll(labelColumn, unitLabelColumn);
+        data.addAll(pm.getProduct());
+        table.setItems(data);
+    }
+
+    public void editedRow() {
+        if (table.getSelectionModel().getSelectedItem() != null) {
+            selectedProduct = (Product) table.getSelectionModel().getSelectedItem();
+            cleanProductTextFieldButton.setVisible(true);
+            addProductButton.setVisible(false);
+            delProductButton.setVisible(true);
+            modProductButton.setVisible(true);
+            inputLabel.setText(selectedProduct.getLabel());
+            inputUnitLabel.setText(selectedProduct.getUnitLabel());
+        }
+    }
 }
--- src/main/java/hu/unideb/inf/warehouse/model/ProductModel.java
+++ src/main/java/hu/unideb/inf/warehouse/model/ProductModel.java
@@ -4,84 +4,62 @@ import hu.unideb.inf.warehouse.pojo.Product;
 import hu.unideb.inf.warehouse.utility.EntityManagerFactoryUtil;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
 
-public class ProductModel {
+public class ProductModel  implements AutoCloseable {
 
-    EntityManagerFactory entityManagerFactory;
-    EntityManager entityManager;
+    private EntityManager entityManager;
 
     public ProductModel() {
-        this.entityManagerFactory  = EntityManagerFactoryUtil.getInstance().getEntityManagerFactory();
-        this.entityManager  = entityManagerFactory.createEntityManager();
+        this.entityManager  = new EntityManagerFactoryUtil().getEntityManager();
     }
-    /**
-     * Letárolja a telephelyet.
-     *
-     */
-    public void addProduct(Object object) {
+
+    public void addProduct(Product Product) {
         try {
             entityManager.getTransaction().begin();
-            entityManager.persist(object);
+            entityManager.persist(Product);
             entityManager.getTransaction().commit();
         } catch (Exception ex){
-            System.out.println("Hiba a/az '"+object.getClass().toString()+"' osztály adatainak betöltésekor:\n");
-        } finally {
-            entityManager.close();
+            System.out.println("Hiba a/az '"+Product.getClass().toString()+"' osztály adatainak betöltésekor:\n");
         }
     }
+
+    public void modProduct(Product Product) {
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.merge(Product);
+            entityManager.getTransaction().commit();
+        } catch (Exception ex){
+            System.out.println("Hiba a/az '"+Product.getClass().toString()+"' osztály adatainak betöltésekor:\n");
+        }
+    }
+
     public List<Product> getProduct() {
         List<Product> list = null;
         try {
-            TypedQuery<Product> query = entityManager.createQuery("SELECT u FROM Product u", Product.class);
+            TypedQuery<Product> query = entityManager.createQuery(
+                    "SELECT u FROM Product u", Product.class);
             list = query.getResultList();
         } catch (Exception ex){
-            System.out.println("Hiba az 'Áru' adatainak lekérdezéskor:\n" + ex);
-        } finally {
-            entityManager.close();
+            System.out.println("Hiba a 'Beszerző' adatainak lekérdezéskor:\n" + ex);
         }
         return list;
     }
 
-    public void removeProduct(Product product) {
+    public void removeProduct(Product Product) {
         try {
             entityManager.getTransaction().begin();
-            entityManager.remove(entityManager.find(Product.class, product.getId()));
+            entityManager.remove(entityManager.find(Product.class, Product.getId()));
             entityManager.getTransaction().commit();
         } catch (Exception ex){
-            System.out.println("Hiba az 'Product' osztály adatainak törlésekor:\n" + ex);
-        } finally {
-            entityManager.close();
+            System.out.println("Hiba az 'Beszerző' adatainak törlésekor:\n" + ex);
         }
     }
 
-//    public List<Product> getProductPerPlace(Place place) {
-//        EntityManager em = emf.createEntityManager();
-//        List<Product> list = null;
-//        try {
-//            TypedQuery<Product> query = em.createQuery("SELECT p FROM UnitPrice p WHERE p.user = :place", Post.class);
-//            query.setParameter("place", place);
-//            list = query.getResultList();
-//        } catch (Exception ex){
-//            System.out.println("Hiba az 'Raktárankénti árúkészlet' adatainak lekérdezéskor:\n" + ex);
-//        } finally {
-//            em.close();
-//        }
-//        return list;
-//    }
-
-//
-//    public List<Post> getLastPostOfEachThread() {
-//        EntityManager em = emf.createEntityManager();
-//        TypedQuery<Post> postQuery = em.createQuery("SELECT p FROM Post p WHERE p.postTime = ("
-//                + "SELECT MAX(p2.postTime) FROM Post p2 WHERE p2.thread = p.thread"
-//                + ")", Post.class);
-//        List<Post> postList = postQuery.getResultList();
-//        em.close();
-//        return postList;
-//
-//    }
+    @Override
+    public void close() {
+        this.entityManager.close();
+    }
 }
